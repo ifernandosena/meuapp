6167001b5ec677fc8fb6fde0d72b5639b9750203
ifernandosena <ifernandosena.a@gmail.com>
2025-02-12 13:55:45 -0300

    fernando
    Log

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LogAlocacao from "./components/LogAlocacao";
+
+
+function App() {
+  return (
+    <div className="App">
+      <LogAlocacao />
+    </div>
+  );
+}
+
+export default App;
